bccb6254f0a78d02ab2e18379fb60228ced32f61
Simar Malhotra <malhotrasimar009@gmail.com>
2026-02-26 08:55:48 -0500

    stable

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "raylib.h"
+#include <raylib.h>
 #include <stdio.h> 
 
 #define TO_SECS 60 
@@ -7,103 +7,114 @@
 #define HEIGHT 500/3
 #define NUM_FRAMES 3  
 
-
 int main(int argc, char* argv[])
 {
-  
-  InitWindow(WIDTH, HEIGHT, "Timer- Lock in! ");
-  InitAudioDevice();
-  Sound fxButton = LoadSound("buttonfx.wav");
-  Texture2D pause_button = LoadTexture("osaka_sticker_re.jpeg");
-  pause_button.height = pause_button.height / 3;
-  pause_button.width = pause_button.width / 3;
-  printf("Pause button height: %f, Pause button width: %f ", (float)pause_button.height, (float)pause_button.width);
+    InitWindow(WIDTH, HEIGHT, "Timer- Lock in!");
+    InitAudioDevice();
 
-  float frameHeight = (float)pause_button.height ;
-  printf("Frame height: %f", frameHeight);
+    Sound fxButton = LoadSound("buttonfx.wav");
+    Sound fxFinished = LoadSound("sata-andagi.mp3");
 
-  /* 
-    typedef struct Rectangle {
-    float x;                // Rectangle top-left corner position x
-    float y;                // Rectangle top-left corner position y
-    float width;            // Rectangle width
-    float height;           // Rectangle height
-    } Rectangle;
-*/
+    bool timerEnded = false;
+    bool finishedPlayed = false;
 
-  Rectangle sourceRec = { 0, 0, (float)pause_button.width /6 , frameHeight };
-  Rectangle btnBounds = {
-    WIDTH/2 + WIDTH/4 ,
-    HEIGHT/2,
-    pause_button.width,
-    pause_button.height
+    Texture2D pause_button = LoadTexture("osaka_sticker_re.jpeg");
+    pause_button.height = pause_button.height / 3;
+    pause_button.width = pause_button.width / 3;
 
-  };
-  
-  int btnState = 0;
-  Vector2 mousePoint = { 0.0f, 0.0f };
-  SetTargetFPS(60);
-  
-  int TIME_SCHEDULED_MIN = 30;
-  if (argc > 1) TIME_SCHEDULED_MIN = atoi(argv[1]);
-  const double TIME_SCHEDULED_SEC = (double)TIME_SCHEDULED_MIN * TO_SECS;
-  
-  double startTime = GetTime();
-  double pausedElapsed = 0.0;
-  bool pause = false;
-  char text[64];
-  
-  while (!WindowShouldClose())
-  {
-    mousePoint = GetMousePosition();
-    if (CheckCollisionPointRec(mousePoint, btnBounds))
+    printf("Pause button height: %f, Pause button width: %f\n", 
+           (float)pause_button.height, (float)pause_button.width);
+
+    float frameHeight = (float)pause_button.height;
+    printf("Frame height: %f\n", frameHeight);
+
+    Rectangle sourceRec = { 0, 0, (float)pause_button.width / 6, frameHeight };
+    Rectangle btnBounds = {
+        WIDTH/2 + WIDTH/4,
+        HEIGHT/2,
+        pause_button.width,
+        pause_button.height
+    };
+
+    int btnState = 0;
+    Vector2 mousePoint = { 0.0f, 0.0f };
+    SetTargetFPS(60);
+
+    float TIME_SCHEDULED_MIN = 30;
+    float TIME_TO_START= 0;
+    if (argc > 1){
+      TIME_SCHEDULED_MIN = atof(argv[1]);
+    } 
+
+    const double TIME_SCHEDULED_SEC = (double)TIME_SCHEDULED_MIN * TO_SECS;
+
+    double startTime = GetTime();
+    double pausedElapsed = 0.0;
+    bool pause = false;
+    char text[64];
+
+    while (!WindowShouldClose())
     {
-      if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
-      {
-        PlaySound(fxButton);
-        pause = !pause;
-        if (pause)
+        mousePoint = GetMousePosition();
+
+        // Pause button logic
+        if (!timerEnded && CheckCollisionPointRec(mousePoint, btnBounds))
         {
-          pausedElapsed = GetTime() - startTime;
-          btnState = 2;
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                PlaySound(fxButton);
+                pause = !pause;
+                if (pause)
+                {
+                    pausedElapsed = GetTime() - startTime;
+                    btnState = 2;
+                }
+                else
+                {
+                    startTime = GetTime() - pausedElapsed;
+                    btnState = 0;
+                }
+            }
         }
-        else
+
+        sourceRec.y = btnState * frameHeight;
+
+        double elapsed = pause ? pausedElapsed : GetTime() - startTime;
+        double remaining = TIME_SCHEDULED_SEC - elapsed;
+
+        // Timer ended logic
+        if (remaining <= 0)
         {
-          startTime = GetTime() - pausedElapsed;
-          btnState = 0;
+            remaining = 0;
+            timerEnded = true;
+
+            if (!finishedPlayed)
+            {
+                PlaySound(fxFinished);
+                finishedPlayed = true;
+            }
         }
-      }
+
+        // Only update text while timer is running
+        snprintf(text, sizeof(text), "Time left: %.0f s", remaining);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText(text, WIDTH / 2 - 75, HEIGHT / 2, 20, BLACK);
+        DrawTexture(pause_button, WIDTH/2 + WIDTH/4, HEIGHT/2, WHITE);
+        EndDrawing();
+
+        // Exit after audio finishes
+        if (timerEnded && finishedPlayed && !IsSoundPlaying(fxFinished))
+            break;
     }
 
-    
-    sourceRec.y = btnState * frameHeight;
-    
-    double elapsed = pause ? pausedElapsed : GetTime() - startTime;
-    double remaining = TIME_SCHEDULED_SEC - elapsed;
-    if (remaining < 0) remaining = 0;
-    
-    snprintf(text, sizeof(text), "Time left: %.0f s", remaining);
-    
-    BeginDrawing();
-    ClearBackground(RAYWHITE);
-    DrawText(text, WIDTH / 2 - 75, HEIGHT / 2, 20, BLACK);
+    // Clean up
+    UnloadTexture(pause_button);  
+    UnloadSound(fxButton);
+    UnloadSound(fxFinished);
+    CloseAudioDevice();
+    CloseWindow();
 
-    // DrawTexture(Texture2D texture, int posX, int posY, Color tint);                               
-    DrawTexture(
-      pause_button, 
-      WIDTH/2 + WIDTH/4 ,
-      HEIGHT/2,
-      WHITE
-    );
-
-    EndDrawing();
-
-  }
-  
-  UnloadTexture(pause_button);  
-  UnloadSound(fxButton);
-  CloseAudioDevice();
-  CloseWindow();
-  
-  return 0;
+    return 0;
 }
